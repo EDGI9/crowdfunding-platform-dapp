5635147b32bd2af1f4d2880a26e49f72a725bd9d
Edgar Dias <dias.s.edgar@gmail.com>
2024-05-14 18:00:48 +0000

    Add function to donate to campaign

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.7.4;
 
 contract CrowdFunding {
     struct Campaign {
@@ -38,8 +38,17 @@ contract CrowdFunding {
             campaign.image = _image;
         } 
 
-        function donateToCampaign() {
-            
+        function donateToCampaign(uint256 _id) public payable  {
+            uint256 amount = msg.value;
+            Campaign storage = campaign campaigns[_id];
+            campaign.donators.push(msg.sender);
+            campaign.donations.push(amount);
+
+            (bool sent, ) = payable(campaign.owner).call{value: amount}("");
+
+            if(sent) {
+                campaign.amountCollected = campaign.amountCollected + amount;
+            } 
         }
 
         function getDonators() {
