5aa6c55af2520c08b40d41ecd95c15d90679d35a
Edgar Dias <dias.s.edgar@gmail.com>
2024-05-14 17:55:06 +0000

    Creating the basic structure for the contract and populating teh create campaign function

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -2,5 +2,52 @@
 pragma solidity ^0.8.9;
 
 contract CrowdFunding {
-    constructor() {}
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amount;
+        string image;
+        address[] donator;
+        uint256[] donations;
+
+        mapping (uint256 => Campaign) public campaigns;
+
+        uint256 public numberOfCampaigns = 0;
+
+        function createCampaign(
+                address owner, 
+                string memory _title, 
+                string memory _description, 
+                uint256 memory _target, 
+                uint256 memory _deadline, 
+                string memory _image) public returns(uint256)   {
+            
+            Campaign storage campaign = campaigns[numberOfCampaigns]
+
+            require(campaign.deadline < block.timestamp, "The dealine should be a date in teh future");
+
+            campaign.owner = _owner;
+            campaign.title = _title;
+            campaign.description = _description;
+            campaign.target = _target;
+            campaign.deadline = _deadline;
+            campaign.amountCollected = 0;
+            campaign.image = _image;
+        } 
+
+        function donateToCampaign() {
+            
+        }
+
+        function getDonators() {
+            
+        }
+
+        function getCampaigns() {
+            
+        }
+    }
 }
