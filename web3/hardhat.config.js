4fa340033d6578fee75f63cf2551192d92deea39
Edgar Dias <dias.s.edgar@gmail.com>
2024-05-19 12:07:07 +0000

    Updating Hardhat configuration

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,33 +1,20 @@
 require("@matterlabs/hardhat-zksync-solc");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  zksolc: {
-    version: "1.3.9",
-    compilerSource: "binary",
-    settings: {
-      optimizer: {
-        enabled: true,
-      },
-    },
-  },
+  defaultNetwork: 'sepolia_testnet',
   networks: {
-    zksync_testnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli",
-      chainId: 280,
-      zksync: true,
-    },
-    zksync_mainnet: {
-      url: "https://zksync2-mainnet.zksync.io/",
-      ethNetwork: "mainnet",
-      chainId: 324,
-      zksync: true,
+    hardhat:{},
+    sepolia_testnet: {
+      url: "https://sepolia.infura.io/v3/",
+      chainId: 11155111,
+      accounts:[`0x${process.env.PRIVATE_KEY}`]
     },
   },
   paths: {
-    artifacts: "./artifacts-zk",
-    cache: "./cache-zk",
+    artifacts: "./artifacts",
+    cache: "./cache",
     sources: "./contracts",
     tests: "./test",
   },
