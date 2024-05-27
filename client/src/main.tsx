45c3671703eb4ade94f7ba8fece431cfcd2c5e50
Edgar Dias <dias.s.edgar@gmail.com>
2024-05-27 17:54:16 +0000

    Removing default files for react app and creating a few from scratch

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,18 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
-import { ThirdwebProvider } from "thirdweb/react";
-import "./index.css";
+import React  from "react";
+import ReactDOM  from "react-dom/client";
+import { BrowserRouter as Router } from 'react-router-dom';
+import {ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
-createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ThirdwebProvider>
-      <App />
+import App from "./App"
+
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <ThirdwebProvider desiredChainId={ChainId.Solana}>
+        <Router>
+            <App />
+        </Router>
     </ThirdwebProvider>
-  </React.StrictMode>
-);
+)
