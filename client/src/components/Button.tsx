ae083ef3fcefb826b2e9fa85d8a2cf8696733a74
Edgar Dias <dias.s.edgar@gmail.com>
2024-06-04 09:27:36 +0000

    Created Button and Navbar components

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Button = ({ btnType, title, handleClick, classes }) => {
+  return (
+    <button
+      data-testid="qa-button"
+      type={btnType}
+      className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${classes}`}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
+  )
+}
+
+export default Button
